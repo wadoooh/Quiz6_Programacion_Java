f96e165f7a839490acdd9d956673489ced1d9434
wadoooh <142348480+wadoooh@users.noreply.github.com>
2023-10-25 22:27:16 -0500

    Update Circulo.java

--- Circulo.java
+++ Circulo.java
@@ -1,1 +1,28 @@
+class  Circulo extends FiguraGeometrica {
 
+    private  double radio;
+
+    public Circulo( String nombre, String color, double radio) {
+        super  (nombre, color);
+        this.radio=radio;
+
+    }
+
+
+    @Override
+    public double obtenerArea() {
+        super.obtenerArea();
+        double area= Math.PI * radio * radio;
+        System.out.println("Area: " + area);
+        return  area;
+    }
+
+
+    @Override
+    public double obtenerPerimetro() {
+        super.obtenerPerimetro();
+        double  perimetro= 2 * Math.PI * radio;
+        System.out.println("Perimetro: " +  perimetro);
+        return  perimetro;
+    }
+}
