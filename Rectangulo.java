ed3fb6ab9e4e612aa1b1f0b6d93e0c04d342db44
wadoooh <142348480+wadoooh@users.noreply.github.com>
2023-10-25 22:29:08 -0500

    Update Rectangulo.java

--- Rectangulo.java
+++ Rectangulo.java
@@ -1,1 +1,27 @@
+class Rectangulo extends FiguraGeometrica {
 
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo(  String nombre, String color, double lado1, double lado2) {
+        super  (nombre, color);
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    @Override
+    public double obtenerArea() {
+        super.obtenerArea();
+        double area= lado1 * lado2;
+        System.out.println("Area: " + area);
+        return  area;
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+        super.obtenerPerimetro();
+        double  perimetro= 2 * (lado1 + lado2);
+        System.out.println("Perimetro: " +  perimetro);
+        return  perimetro;
+    }
+}
