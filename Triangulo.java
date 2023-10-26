9f09c722f32cf5186d2b5f3ac881b7082ab90b7e
wadoooh <142348480+wadoooh@users.noreply.github.com>
2023-10-25 22:29:24 -0500

    Update Triangulo.java

--- Triangulo.java
+++ Triangulo.java
@@ -1,1 +1,26 @@
+class Triangulo extends FiguraGeometrica {
+    private double base;
+    private double altura;
 
+    public Triangulo( String nombre, String color, double base, double altura) {
+        super  (nombre, color);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double obtenerArea() {
+        super.obtenerArea();
+        double area=   (base * altura)/2;
+        System.out.println("Area: " + area);
+        return  area;
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+        super.obtenerPerimetro();
+        double  perimetro= base*3;
+        System.out.println("Perimetro: " +  perimetro);
+        return  perimetro;
+    }
+}
