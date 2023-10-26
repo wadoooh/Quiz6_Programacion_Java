8b0dad771eb48d772a4113097f04932d0be2479c
wadoooh <142348480+wadoooh@users.noreply.github.com>
2023-10-25 22:31:18 -0500

    Update FiguraGeometrica.java

--- FiguraGeometrica.java
+++ FiguraGeometrica.java
@@ -1,1 +1,26 @@
+import java.awt.geom.Area;
 
+
+class FiguraGeometrica {
+
+    private String nombre;
+    private String color;
+
+
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public double obtenerArea() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Color: " + color);
+        return 0.0;
+    }
+
+    public double obtenerPerimetro() {
+        return 0.0;
+    }
+
+}
